ec39525d16909a6a286c3016298ad1202b2c7974
AlmadaLuiz14 <luiz.almada3@gmail.com>
2023-09-28 00:15:53 -0300

    login

--- src/views/Cad_screen.vue
+++ src/views/Cad_screen.vue
@@ -1,52 +1,53 @@
-<template>
-    <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">  
-    </head>
-        <body>
-            
-            <div class='container'>
-                <div class='card'>
-                    <h1> Cadastrar </h1>
-                    
-                    <form id="cadastro" @submit.prevent="cadastrar">
+<template>            
+    <div class='container'>
+        <div>
+            <h1 class="card">Cadastre-se e seja um</h1>
+            <h1 class="card1">membro FutStats!</h1>
 
-                        <div id='msgError'></div>
-                        <div id='msgSuccess'></div>
-                        
-                        <div class="label-float">
-                            <label id="labelNome" for="nome">Nome</label>
-                            <input type="text" id="nome" name="nome" v-model="nome" placeholder=" "  required>
-                        </div>
-            
-                        <div class="label-float">
-                            <label id="labelEmail" for="email">Email</label>
-                            <input type="text" id="email" name="email" v-model="email" placeholder=" " required>
-                        </div>
-                        
-                        <div class="label-float">
-                            <label id="labelSenha" for="senha">Senha</label>
-                            <input type="password" id="senha" name="senha" v-model="senha" placeholder=" " required>
-                            <i id="verSenha" class="fa fa-eye" aria-hidden="true"></i>
-                        </div>
-            
-                        <div class="label-float">
-                            <label id="labelConfirmSenha" for="confirmSenha">Confirmar Senha</label>
-                            <input type="password" id="confirmSenha" name="confirmSenha" v-model="confirmSenha" placeholder=" " @input="checarSenhas" required>
-                            <i id="verConfirmSenha" class="fa fa-eye" aria-hidden="true"></i>
-                        </div>
-                        
-                        <div class='justify-center'>
-                            <!--<button onclick='cadastrar()'>Cadastrar</button>-->
-                            <input type="submit" class="submit-btn" value="Cadastrar">
-                            <p v-if="senhasDiferentes" class="verifSenhas">As senhas não coincidem</p>
-                        </div>
+            <form id="cadastro" @submit.prevent="cadastrar">
+                <div id='msgError'></div>
+                <div id='msgSuccess'></div>
                 
-                    </form>
-
+                <div class="label-float">
+                    <label id="labelNome" for="nome">Nome</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder=" "  required>
                 </div>
-            </div>
-            
-        </body>
+    
+                <div class="label-float">
+                    <label id="labelEmail" for="email">Email</label>
+                    <input type="text" id="email" name="email" v-model="email" placeholder=" " required>
+                </div>
+                
+                <div class="label-float">
+                    <label id="labelSenha" for="senha">Senha</label>
+                    <input type="password" id="senha" name="senha" v-model="senha" placeholder=" " required>
+                    <i id="verSenha" class="fa fa-eye" aria-hidden="true"></i>
+                </div>
+    
+                <div class="label-float confirme">
+                    <label id="labelConfirmSenha" for="confirmSenha">Confirmar Senha</label>
+                    <input type="password" id="confirmSenha" name="confirmSenha" v-model="confirmSenha" placeholder=" " @input="checarSenhas" required>
+                    <i id="verConfirmSenha" class="fa fa-eye" aria-hidden="true"></i>
+                </div>
+                
+                <div class='justify-center'>
+                    <!--<button onclick='cadastrar()'>Cadastrar</button>-->
+                    <input type="submit" class="submit-btn" value="Cadastrar">
+                    <p v-if="senhasDiferentes" class="verifSenhas">As senhas não coincidem</p>
+                </div>
+        
+            </form>
+        </div>
+        <div class="additional-card">
+            <h2>Por que ser um membro FutStats?</h2>
+            <p>Ao se juntar ao nosso grupo,</p> 
+            <p>você terá acesso a notícias</p>
+            <p>exclusivas</p>
+            <p>sobre o Brasileirao, além de</p>
+            <p> poder interagir comentando.</p>
+            <img src="../assets/TrofeuLogo.png" alt="Troféu FutStats">
+        </div>
+    </div>
 </template>
 
 <script>
@@ -90,6 +91,8 @@
                     this.email = ""
                     this.senha = ""
                     this.confirmSenha = "" 
+
+                    alert("Cadastro realizado com sucesso!")
                 }
             },
 
@@ -105,5 +108,123 @@
 
 </script>
 
+
 <style scoped>
+.container {
+  width: 120vh;
+  height: 100vh;
+  opacity: 0.93;
+  background: #C0C0C0;
+  padding: 25px;
+  align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: left;
+}
+.card{
+  position: absolute;
+  top: 240px;
+  left: 350px;
+  font-family: 'Inter';
+  font-size: 36px;
+}
+.card1{
+  position: absolute;
+  top: 280px;
+  left: 370px;
+  font-family: 'Inter';
+  font-size: 36px;
+}
+
+.label-float {
+  width: 80vh;
+  height: 30px;
+  top: 190px;
+  left: 184px;
+  position: relative;
+  margin-bottom: 20px;
+}
+
+.label-float label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-size: 16px;
+  color: #333;
+  pointer-events: none;
+  transition: 0.2s ease all;
+  padding-left: 5px;
+}
+
+.label-float input {
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  outline: none;
+  margin-top: 20px;
+}
+
+.submit-btn {
+  background: #007BFF;
+  color: #fff;
+  font-size: 16px;
+  padding: 10px 250px;
+  margin-top: 250px;
+  margin-left: 185px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.2s ease background-color;
+}
+
+.submit-btn:hover {
+  background-color: #0056b3;
+}
+
+.fa-eye {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.verifSenhas {
+  color: red;
+  font-size: 24px;
+  margin-top: 10px;
+  margin-left: 200px;
+  text-align: center
+}
+
+.additional-card {
+  height: 103vh;
+  width: 76vh;
+  top: 5vh;
+  left: 126vh;
+  padding: 15px;
+  background: #D8D8D8;
+  text-align: center;
+  position: absolute;
+}
+
+.additional-card h2 {
+  font-size: 40px;
+  margin-bottom: 30px;
+  font-family: 'Inter';
+}
+
+.additional-card p {
+  font-size: 24px;
+  margin-bottom: 10px;
+  font-family: 'Inter';
+}
+
+.additional-card img {
+  margin-top: 50px;
+  max-width: 50%;
+  height: auto;
+}
 </style>
--- src/views/Login_screen.vue
+++ src/views/Login_screen.vue
@@ -1,69 +1,152 @@
 <template>
-    <div class="coluna1">
-        <h1 class="ls-login-logo">Login</h1>
-        <form role="form">
-                <div class="form-group ls-login-user">
-                    <label for="userLogin">Usuário</label>
-                    <input class="form-control ls-login-bg-user input-lg" id="userLogin" type="text" aria-label="Usuário" placeholder="Usuário">
+    <div class="container">
+        <div class="card">
+            <h1 class="ls-login-logo">Login</h1>
+            <form id="login" @submit.prevent="login">
+                <div class="form-group label-float">
+                    <label for="userEmail">E-mail</label>
+                    <input class="form-control ls-login-bg-user input-lg" id="userEmail" name="userEmail" type="text" aria-label="Usuário" placeholder="E-mail" v-model="email" required>
                 </div>
             
-                <div class="form-group ls-login-password">
+                <div class="form-group label-float">
                     <label for="userPassword">Senha</label>
-                    <input class="form-control ls-login-bg-password input-lg" id="userPassword" type="password" aria-label="Senha" placeholder="Senha">
+                    <input class="form-control ls-login-bg-password input-lg" id="userPassword" name="userPassword" type="password" aria-label="Senha" placeholder="Senha" v-model="senha" required>
                 </div>
 
                 <!--<a href="#" class="ls-login-forgot">Esqueci minha senha</a>-->
 
-                <input type="submit" value="Entrar" class="btn btn-primary btn-lg btn-block">
+                <input type="submit" class="submit-btn" value="Entrar">
                 <p class="txt-center ls-login-signup">Não possui um usuário no FutStats?
                     <!--<a href="#">Cadastre-se agora</a>-->
                 </p>
-        </form>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
-    import usuarios from '../db/db.json'
 
-    export default{
-        data(){
-            return{
-                form:{
-                    email: null,
-                    senha: null
-                }
+    export default {
+        nome: "usuarios",
+        data() {
+            return {
+                email: null,
+                senha: null,
             }
         },
 
-        methods:{
-            login(){
-                const user = usuarios.find(user => user.email === this.email)
+        methods: {
+            async login(e){
+                e.preventDefault()
 
-                if(user && user.senha === this.senha){
-                    alert("Login bem-sucedido!")
-                }else{
-                    alert("Não foi possível fazer o login")
+                /*fetch("http://localhost:3000/usuarios")
+                    .then(res => res.json())
+                    .then(data => this.contas = data)
+                    .catch(err => console.log(err.message))*/
+                
+                
+                const data = await fetch("http://localhost:3000/usuarios", {
+                    method: "GET",
+                    headers: {"Content-Type": "application/json"}
+                })
+                
+                const contas = await data.json()
+
+                for(let i = 0; i < contas.length; i++){
+                    if(contas[i]["email"] === this.email && contas[i]["senha"] === this.senha){
+                        alert("Seja bem-vindo!")
+                        break
+                    }
                 }
-            }
 
+
+            }
         }
     }
 
 </script>
 
-<style>
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
-    *,
-    *::after,
-    *::before{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        text-decoration: none;
-    }
+.card {
+  background: #d4d4d4;
+  border-radius: 10px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 400px;
+}
 
-    .coluna1{
-        margin-left: 0; 
-    }
+.label-float {
+  position: relative;
+  margin-bottom: 40px;
+  margin-right: 35px;
+  
+}
 
+.label-float label {
+  position: absolute;
+  left: 0;
+  top: -24%;
+  transform: translateY(-50%);
+  pointer-events: none;
+  transition: 0.3s ease-out all;
+}
+
+.label-float input {
+  width: 100%;
+  background: #dddddd;
+  padding: 15px;
+  font-size: 16px;
+  border: 1px solid #000000;
+  border-radius: 5px;
+  outline: none;
+}
+
+.label-float input:focus {
+  border-color: #007BFF;
+}
+
+.label-float input:not(:placeholder-shown) + label {
+  top: 0;
+  font-size: 12px;
+  color: #007BFF;
+}
+
+.submit-btn {
+  background-color: #007BFF;
+  color: #fff;
+  font-size: 16px;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 100%;
+  transition: background-color 0.3s ease;
+}
+
+.submit-btn:hover {
+  background-color: #0056b3;
+}
+
+.verSenha,
+.verConfirmSenha {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.verifSenhas {
+  color: red;
+  font-size: 14px;
+  margin-top: 10px;
+  text-align: center;
+}
 </style>
